ef1037664a33ec2eec207c0324e591d27656079b
darkmian939 <140548399+darkmian939@users.noreply.github.com>
2024-09-02 16:13:54 -0500

    Feat(Api); guardo cambios de la Api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // Importa el middleware cors
 require('dotenv').config();
 
 const app = express();
@@ -8,12 +9,18 @@ const port = process.env.PORT || 5000;
 // Middleware para analizar JSON
 app.use(express.json());
 
+// Configuración de CORS para permitir solicitudes desde el frontend
+app.use(cors({
+  origin: 'http://localhost:3000', // Permite solicitudes desde tu frontend en el puerto 3000
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
+}));
+
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
   .catch((error) => console.error('Error al conectar a MongoDB:', error));
 
-  
 // Importar rutas
 const aprendizRoutes = require('./routes/aprendizRoutes');
 
