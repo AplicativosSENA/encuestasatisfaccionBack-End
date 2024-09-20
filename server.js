642327b02188d69138128232cc91470b00bec65c
darkmian939 <140548399+darkmian939@users.noreply.github.com>
2024-09-19 23:29:15 -0500

    guardo cambios

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('Conectado a MongoDB'))
   .catch((error) => console.error('Error al conectar a MongoDB:', error));
 
+  app.get('/', (req, res) => {
+    res.send('API funcionando correctamente');
+  });
+
+  
 // Importar rutas
 const aprendizRoutes = require('./routes/aprendizRoutes');
 const instructorRoutes = require('./routes/instructorRoutes'); // Asegúrate de este path
